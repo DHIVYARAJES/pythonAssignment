52f0633f1cde248855b287e068576fb8f1a7aacb
Dhivya Jagannathan <shravan.k.git@gmail.com>
2019-03-19 14:20:28 +0530

    homework2

---
+++ main.py
@@ -0,0 +1,37 @@
+# homework
+# assignment functions
+#
+# def fav_song(name1 ,name2 ,name3):
+#    name = print("Album is a  " +name1)
+#    name = print("Artist is " +name2)
+#    name = print("relaesed in the year " +name3)
+#    return name
+#
+#
+# fav_song("pop", "nick", "2012")
+#
+# fav_song("melodey","rahman","2000")
+#
+
+def genere(name1):
+    print("the genere is " +name1)
+    return name1
+
+def artist(name1):
+    print("artist is " +name1)
+    return name1
+
+def year(name1):
+    print("released year is " +name1)
+    return name1
+
+def boolean(demo):
+    if demo:
+         print("the value passed is true")
+    return demo
+
+
+genere("pop")
+artist("nick")
+year("2000")
+boolean(True)#if False it wont be passed
